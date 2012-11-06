d537dcac91a0a50e884d83040ba630e7d1f3ff2d
LastGenius <8kaposelenie2010@inbox.ru>
2012-11-06 20:52:36 +0400

    Four

--- __main__.py
+++ __main__.py
@@ -1,15 +1,19 @@
 from auth import auth
 from lxml import html
 import urllib2
+import vkontakte
 
-email = 'OLOLO' # почта от акка
-password = 'OLOLO' #пароль от акка
+email = '123' # 
+password = '123' #
 vkID = '2951857'
 scope = 'audio'
 
-#tehToken = 'a85ad24df9e5dc9aa92044ed47a95252748a97fa97f58cdf9e66a1a1e68cd1e8f4e2473'
+#tehToken = 'e103d0a5b0bcca78e09654dbd5e00b509cce026e0265a25b0bf64f889a2f6da22b9f40a'
+#userID = 19237512
 tehToken, userID = auth(email, password, vkID, scope)
-print(tehToken)
+print(tehToken, userID)
+vk = vkontakte.API(token=tehToken)
+sresult = vk.audio.search(q='Mozart', count=5)
 tehURL = "https://api.vkontakte.ru/method/audio.get.xml?uid=" + userID + "&access_token=" + tehToken
 page = urllib2.urlopen(tehURL)
 rawHTML = page.read()
@@ -18,12 +22,9 @@ try:
 except html.etree.ParserError:
     print('Parser error.')
 
-#doc  = html.document_fromstring(tehURL)
-
 artistMas = []
 number = 0
 
-print(doc.cssselect('artist').__len__())
 for artist in doc.cssselect('artist'):
     artistMas.append(artist.text)
     number += 1
